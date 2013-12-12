b688f4cdc0fa67ffe950a4e3db211d0dfff5e16b
Taylor Hakes <taylorhakes@taylors-mac-pro.home>
2013-12-12 12:17:16 -0500

    Added a few unit tests to main controller

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+	  'app/bower_components/ngInfiniteScroll/ng-infinite-scroll.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
@@ -46,7 +47,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,17 +2,79 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('etsyApp'));
+	// load the controller's module
+	beforeEach(module('etsyApp'));
 
-  var MainCtrl,
-    scope;
+	var MainCtrl,
+		scope,
+		mockFavoriteService;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		mockFavoriteService = {
+			callback: function () {
+			},
+			save: function (item) {
+				this.callback('save', item);
+			},
+			remove: function (item) {
+				this.callback('remove', item);
+			},
+			getAll: function() {
+				return {};
+			}
+		};
+
+		MainCtrl = $controller('MainCtrl', {
+			$scope: scope,
+			favoriteService: mockFavoriteService
+		});
+	}));
+
+	describe('Initial Setup', function () {
+		it('Expect the items to be empty before a search', function () {
+			expect(scope.items).toBe(null);
+		});
+		it('Expect the text to say no items', function () {
+			expect(scope.tempText.indexOf('No Items') > -1).toBeTruthy();
+		});
+		it('Expect the search to be empty', function () {
+			expect(scope.search).toBeFalsy();
+		});
+	});
+
+	describe('Favorite Items', function () {
+		it('Adding a favorite', function () {
+			var called = false;
+			mockFavoriteService.callback = function (method) {
+				if (method === 'save') {
+					called = true;
+				}
+			};
+
+			scope.items = [
+				{ isFavorite: false },
+				{}
+			];
+			scope.onFavorite(0);
+			expect(called).toBe(true);
+		});
+		it('Removing a favorite', function () {
+			var called = false;
+			mockFavoriteService.callback = function (method) {
+				if (method === 'remove') {
+					called = true;
+				}
+			};
+
+			scope.items = [
+				{ isFavorite: true },
+				{}
+			];
+			scope.onFavorite(0);
+			expect(called).toBe(true);
+		});
+	});
+
 });
